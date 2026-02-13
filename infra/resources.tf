13e22fb87cc69f08434ded52bbb7a66c3e269c8b
Vijayrajdev <vijayrajck@outlook.com>
2026-02-13 11:05:07 +0530

    Feat: updating the yml for github.

--- infra/resources.tf
+++ infra/resources.tf
@@ -3,7 +3,7 @@ resource "google_bigquery_dataset" "layers" {
   for_each   = toset(["raw_landing", "audit_logs", "refined_layer",])
   dataset_id = "sentinel_${each.key}"
   location   = "US"
-  delete_contents_on_destroy = true # Be careful with this in production!
+  delete_contents_on_destroy = true 
 }
 
 # 2. Service Account for dbt
