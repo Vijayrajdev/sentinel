aa29bcbf8f9e9608891695b45d52ac3548f2c07f
Vijayrajdev <vijayrajck@outlook.com>
2026-03-07 12:45:09 +0530

    Updates: Updated the Agent names and the instruction to make Agent better.

--- infra/variables.tf
+++ infra/variables.tf
@@ -90,7 +90,3 @@ variable "composer_bucket_name" {
   description = "Name of the existing Composer bucket"
   type        = string
 }
-variable "insights_audit" {
-  description = "Name of the Insights Audit table"
-  type        = string
-}
